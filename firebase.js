2852689252b52aaace719e073f46437c8e1509a4
talkhausdevs <talkhausdevs@gmail.com>
2025-04-17 20:34:26 -0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgFzQHL_q02mGF083TkLREpFyU0dE3Hcs",
+  authDomain: "husic-6705e.firebaseapp.com",
+  projectId: "husic-6705e",
+  storageBucket: "husic-6705e.appspot.com",
+  messagingSenderId: "311183107893",
+  appId: "1:311183107893:web:c62720409ce8669b64ce7c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
